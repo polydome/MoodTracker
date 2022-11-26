48802e0145e4ff0adc6989ffb23d84b8042fe181
polydome <polydome@outlook.com>
2022-11-26 14:40:52 +0100

    Fix package

--- app/src/main/kotlin/com/github/polydome/ui/Main.kt
+++ app/src/main/kotlin/com/github/polydome/ui/Main.kt
@@ -1,6 +1,7 @@
+package com.github.polydome.ui
+
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import com.github.polydome.ui.App
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
