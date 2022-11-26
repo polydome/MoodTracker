bacce7475cc2bcf0c592836ba38cc4176068bbc0
polydome <polydome@outlook.com>
2022-11-26 18:31:11 +0100

    Refactor mood picker out of mood prompt

---
+++ app/src/main/kotlin/com/github/polydome/ui/mood_prompt/MoodPicker.kt
@@ -0,0 +1,78 @@
+package com.github.polydome.ui.mood_prompt
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Surface
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.github.polydome.ui.mood_prompt.icons.*
+
+@Composable
+fun MoodPicker(onMoodPicked: (value: Int) -> Unit) {
+    var selectedMoodIndex: Int? by remember { mutableStateOf(null) }
+
+    Row {
+        MoodLevel.all.forEachIndexed { index, moodLevel ->
+            MoodButton(
+                onClick = {
+                    selectedMoodIndex = index
+                    onMoodPicked(index)
+                },
+                icon = moodLevel.icon,
+                selected = selectedMoodIndex == index
+            )
+        }
+    }
+}
+
+private data class MoodLevel(
+    val value: Int,
+    val icon: ImageVector
+) {
+    init {
+        require(value in 0..5)
+    }
+
+    companion object {
+        val all: Array<MoodLevel>
+            get() = arrayOf(
+                MoodLevel(0, MoodIcons.Mood0),
+                MoodLevel(1, MoodIcons.Mood1),
+                MoodLevel(2, MoodIcons.Mood2),
+                MoodLevel(3, MoodIcons.Mood3),
+                MoodLevel(4, MoodIcons.Mood4),
+            )
+    }
+}
+
+@Composable
+private fun MoodButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    selected: Boolean = false,
+    icon: ImageVector
+) {
+    Surface(
+        shape = CircleShape,
+        modifier = modifier
+    ) {
+        Box(
+            modifier = Modifier.background(if (selected) Color.LightGray else Color.Transparent)
+        ) {
+            IconButton(
+                onClick = onClick
+            ) {
+                Icon(
+                    imageVector = icon,
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
