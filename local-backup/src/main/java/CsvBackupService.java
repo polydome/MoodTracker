f74232e969df53eb122e627f2ddb7e82580854f7
PanTallon <dawdrzew@gmail.com>
2022-11-28 00:18:27 +0100

    Import/eksport do pliku CSV/JSON

--- local-backup/src/main/java/CsvBackupService.java
+++ local-backup/src/main/java/CsvBackupService.java
@@ -1,16 +1,33 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.github.polydome.backup.BackupService;
 import com.github.polydome.model.MoodEntry;
 
+import lombok.SneakyThrows;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
+import javax.annotation.processing.SupportedAnnotationTypes;
+
 public class CsvBackupService implements BackupService {
+
+    private static final String filepath="csv.csv";
     /**
      * Serializes entries into a CSV format and writes to a file.
      * @param entries Mood entries to be backed up
      */
     @Override
+    @SneakyThrows
     public void write(List<MoodEntry> entries) {
-        throw new InternalError("Not implemented");
+        CsvMapper objectMapper=new CsvMapper();
+        String jsonString=objectMapper.writeValueAsString(entries);
+        Path fPath=Paths.get(filepath);
+        Files.writeString(fPath, jsonString);
     }
 
     /**
@@ -18,7 +35,14 @@ public class CsvBackupService implements BackupService {
      * @return A list of backed up entries
      */
     @Override
+    @SneakyThrows
     public List<MoodEntry> read() {
-        throw new InternalError("Not implemented");
+        Path path = Paths.get(filepath);
+        String json = Files.readString(path);
+        CsvMapper objCsvMapper=new CsvMapper();
+
+        return objCsvMapper.readValue(json, new TypeReference<List<MoodEntry>>() {
+   });
     }
 }
+
--- local-backup/src/main/java/JsonBackupService.java
+++ local-backup/src/main/java/JsonBackupService.java
@@ -1,16 +1,31 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.github.polydome.backup.BackupService;
 import com.github.polydome.model.MoodEntry;
 
+import lombok.SneakyThrows;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class JsonBackupService implements BackupService {
+
+    private static final String filepath="json.json";
+    
     /**
      * Serializes entries into a JSON format and writes to a file.
      * @param entries Mood entries to be backed up
      */
     @Override
+    @SneakyThrows
     public void write(List<MoodEntry> entries) {
-        throw new InternalError("Not implemented");
+        ObjectMapper objectMapper=new ObjectMapper();
+        String jsonString=objectMapper.writeValueAsString(entries);
+        Path fPath=Paths.get(filepath);
+        Files.writeString(fPath, jsonString);
     }
 
     /**
@@ -18,7 +33,13 @@ public class JsonBackupService implements BackupService {
      * @return A list of backed up entries
      */
     @Override
+    @SneakyThrows
     public List<MoodEntry> read() {
-        throw new InternalError("Not implemented");
-    }
+         Path path = Paths.get(filepath);
+         String json = Files.readString(path);
+         ObjectMapper objjsonMapper=new ObjectMapper();
+
+        return objjsonMapper.readValue(json, new TypeReference<List<MoodEntry>>() {
+    });
+}
 }
