2bdc1d61ea83d1f5d854630641f367a6a03dd6e1
polydome <polydome@outlook.com>
2022-11-27 12:36:20 +0100

    Add settings button

--- app/src/main/kotlin/com/github/polydome/ui/app/App.kt
+++ app/src/main/kotlin/com/github/polydome/ui/app/App.kt
@@ -3,19 +3,21 @@ package com.github.polydome.ui.app
 import androidx.compose.animation.Crossfade
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.github.polydome.ui.widget.ActionButton
-import com.github.polydome.ui.widget.Header
 import com.github.polydome.ui.calendar.CalendarViewModel
 import com.github.polydome.ui.calendar.MoodCalendar
 import com.github.polydome.ui.mood_prompt.MoodPrompt
 import com.github.polydome.ui.mood_prompt.MoodPromptViewModel
+import com.github.polydome.ui.settings.SettingsButton
+import com.github.polydome.ui.settings.SettingsViewModel
+import com.github.polydome.ui.widget.ActionButton
+import com.github.polydome.ui.widget.Header
 
 enum class Tab {
     Button,
@@ -26,32 +28,41 @@ enum class Tab {
 @Preview
 fun App() {
     MaterialTheme {
-        Column(Modifier.padding(64.dp)) {
-            var tab by remember { mutableStateOf(Tab.Button) }
+        Box {
+            SettingsButton(
+                modifier = Modifier
+                    .align(Alignment.TopEnd),
+                SettingsViewModel()
+            )
 
-            MoodCalendar(CalendarViewModel())
+            Column(Modifier.padding(64.dp)) {
+                var tab by remember { mutableStateOf(Tab.Button) }
 
-            Box {
-                Crossfade(targetState = tab, modifier = Modifier.align(Alignment.Center)) { currentTab ->
-                    when (currentTab) {
-                        Tab.Button -> ButtonView(
-                            modifier = Modifier
-                                .fillMaxSize(),
-                            switchTab = {
-                                tab = Tab.Prompt
-                            }
-                        )
-                        Tab.Prompt -> MoodPrompt(
-                            modifier = Modifier
-                                .fillMaxSize(),
-                            switchTab = {
-                                tab = Tab.Button
-                            },
-                            MoodPromptViewModel()
-                        )
+                MoodCalendar(CalendarViewModel())
+
+                Box {
+                    Crossfade(targetState = tab, modifier = Modifier.align(Alignment.Center)) { currentTab ->
+                        when (currentTab) {
+                            Tab.Button -> ButtonView(
+                                modifier = Modifier
+                                    .fillMaxSize(),
+                                switchTab = {
+                                    tab = Tab.Prompt
+                                }
+                            )
+                            Tab.Prompt -> MoodPrompt(
+                                modifier = Modifier
+                                    .fillMaxSize(),
+                                switchTab = {
+                                    tab = Tab.Button
+                                },
+                                MoodPromptViewModel()
+                            )
+                        }
                     }
                 }
             }
+
         }
     }
 }
---
+++ app/src/main/kotlin/com/github/polydome/ui/settings/SettingsViewModel.kt
@@ -0,0 +1,16 @@
+package com.github.polydome.ui.settings
+
+class SettingsViewModel {
+    fun exportToJson() {
+
+    }
+
+    fun exportToCsv() {
+
+    }
+
+    fun exportToGoogleDrive() {
+
+    }
+
+}
