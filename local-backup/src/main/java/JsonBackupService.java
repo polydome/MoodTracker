b315d3edbc950fbcc2afb7593fd5f531f4aae6bd
polydome <polydome@outlook.com>
2022-11-29 14:13:26 +0100

    Fix lombok processing and exception handling

--- local-backup/src/main/java/JsonBackupService.java
+++ local-backup/src/main/java/JsonBackupService.java
@@ -1,11 +1,11 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.github.polydome.backup.BackupService;
 import com.github.polydome.model.MoodEntry;
 
-import lombok.SneakyThrows;
-
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,33 +13,59 @@ import java.util.List;
 
 public class JsonBackupService implements BackupService {
 
-    private static final String filepath="json.json";
-    
+    private static final String filepath = "json.json";
+    private final ObjectMapper mapper = buildMoodEntryMapper();
+
     /**
      * Serializes entries into a JSON format and writes to a file.
+     *
      * @param entries Mood entries to be backed up
      */
     @Override
-    @SneakyThrows
     public void write(List<MoodEntry> entries) {
-        ObjectMapper objectMapper=new ObjectMapper();
-        String jsonString=objectMapper.writeValueAsString(entries);
-        Path fPath=Paths.get(filepath);
-        Files.writeString(fPath, jsonString);
+        String jsonString;
+
+        try {
+            jsonString = mapper.writeValueAsString(entries);
+        } catch (JsonProcessingException e) {
+            throw new BackupException("File processing failed", e);
+        }
+
+        Path fPath = Paths.get(filepath);
+
+        try {
+            Files.writeString(fPath, jsonString);
+        } catch (IOException e) {
+            throw new BackupException("File writing failed", e);
+        }
     }
 
     /**
      * Reads a JSON file and deserializes its content.
+     *
      * @return A list of backed up entries
      */
     @Override
-    @SneakyThrows
     public List<MoodEntry> read() {
-         Path path = Paths.get(filepath);
-         String json = Files.readString(path);
-         ObjectMapper objjsonMapper=new ObjectMapper();
+        Path path = Paths.get(filepath);
 
-        return objjsonMapper.readValue(json, new TypeReference<List<MoodEntry>>() {
-    });
-}
+        String json;
+        try {
+            json = Files.readString(path);
+        } catch (IOException e) {
+            throw new BackupException("File reading failed", e);
+        }
+
+        try {
+            return mapper.readValue(json, new TypeReference<>() {});
+        } catch (JsonProcessingException e) {
+            throw new BackupException("File deserialization failed", e);
+        }
+    }
+
+    private static ObjectMapper buildMoodEntryMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
 }
