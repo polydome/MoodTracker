e6b4c54798c8f7483021cf2efa4ebfef3df42c54
polydome <polydome@outlook.com>
2022-11-30 12:03:32 +0100

    Fix notices alignment

--- app/src/main/kotlin/com/github/polydome/ui/settings/NoticesView.kt
+++ app/src/main/kotlin/com/github/polydome/ui/settings/NoticesView.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
@@ -31,7 +32,8 @@ fun NoticesView(modifier: Modifier = Modifier, newNotices: Flow<String>) {
 
     LazyColumn(
         modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.End
     ) {
         for (notice in visibleNotices) {
             item {
